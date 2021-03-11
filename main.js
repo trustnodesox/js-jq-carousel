3879eee09e54402dcd1cdcda9643023b0e348bcd
Lorenzo De Sossi <lollodesossi@gmail.com>
2021-03-12 00:34:37 +0100

    completo

--- main.js
+++ main.js
@@ -1,15 +1,44 @@
-var left = $('.fa-angle-left')
-var right = $('fa-angle-right')
-var img = $('img')
-var active = $('.active')
+var left = $('.prev');
+var right = $('.next');
+var last = $('.last');
+var first = $(".first");
 
-if (right.click()) {
-  img.removeClass('active').next().addCLass(active);
-} else if (left.click()){
-  img.removeClass('active').prev().addClass(active)
-}
+//if (right.click()) {
+  //img.removeClass('active').next().addCLass('active');
+//} else if (left.click()){
+//  img.removeClass('active').prev().addClass('active')
+//}
+
 
 
 left.click(function(){
-  img.addClass(active)
+  previous();
 })
+
+right.click(function(){
+  next();
+})
+
+//img.removeClass('active').next().addClass('active');
+
+function previous(){
+  var currentImg = $(".active");
+  var prevImg = currentImg.prev();
+  if(currentImg.hasClass("first")){
+    prevImg = last;
+  }
+  currentImg.removeClass("active");
+  prevImg.addClass("active");
+
+
+}
+
+function next(){
+  var currentImg = $(".active");
+  var nextImg = currentImg.next();
+  if(currentImg.hasClass("last")){
+    nextImg = first;
+  }
+  currentImg.removeClass("active");
+  nextImg.addClass("active");
+}
